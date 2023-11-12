94bd975985ea01f704a0b426038934007cf25067
Your Name <you@example.com>
2023-11-12 23:48:20 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 import "./App.css";
+import React from "react";
+import companiesData from './companies.json';
+import technologiesData from './technologies.json';
+import Navbar from './components/Navbar';
+import HomePage   from './pages/HomePage';
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+  function App() {
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    const companies= companiesData;
+    const technologies= technologiesData;
   return (
+    
     <div className="App">
-      <h1>LAB | React Stack Tracker</h1>
+<Navbar/>
+<React.StrictMode>
+
+<Routes>
+        <Route path="/HomePage" element={<HomePage />} /> 
+        <Route path="/technologies" element={<technologies />} />
+      <Route path="/companies.json" element={<companies />} />
+      </Routes>
+      </React.StrictMode>
+
+<HomePage/>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
+/* eslint-disable no-undef */
+/* eslint-disable react/jsx-no-undef */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter as Router } from "react-router-dom";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+        <Router>
     <App />
-  </React.StrictMode>,
-)
+    </Router>
+    <ul>
+<Link to={`/App.jsx ${companies.json}`}>
+<img src={companies.logo} alt={companies.name} />
+</Link>
+
+</ul>
+
+  </React.StrictMode>
+
+);
